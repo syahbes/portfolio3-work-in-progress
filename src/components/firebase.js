79deb4ccfbe46193fd96ee2149e96c00eec85ee3
syahbes <syahbes@gmail.com>
2023-04-25 19:07:06 +0300

    env

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC52TMVowehP8KY4HuANkWP3ANy9Q6p41I',
-  authDomain: 'react-portfolio-dashboar-8c3bd.firebaseapp.com',
-  projectId: 'react-portfolio-dashboar-8c3bd',
-  storageBucket: 'react-portfolio-dashboar-8c3bd.appspot.com',
-  messagingSenderId: '766562816977',
-  appId: '1:766562816977:web:e9234aae5435cdaef186f0',
-}
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
